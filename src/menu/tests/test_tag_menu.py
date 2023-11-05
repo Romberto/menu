253989c53e563a96929fc70edae484c81855c9a8
romberto <korhoff@mail.ru>
2023-11-05 16:47:03 +0400

    "tests"

--- src/menu/tests/test_tag_menu.py
+++ src/menu/tests/test_tag_menu.py
@@ -1,18 +1,22 @@
-from pprint import pprint
 from unittest import TestCase
 
-from menu.models import Menu, MenuItem
-from menu.templatetags.menu_tag import draw_menu
+from ..models import Menu, MenuItem
+from ..templatetags.menu_tag import draw_menu, build_menu_structure
 
 
-class TestTagMenu(TestCase):
+class TestMenuTag(TestCase):
+
     def setUp(self):
         self.menu = Menu.objects.create(name="title-1")
         self.item_menu_1 = MenuItem.objects.create(title="item_title_1", menu=self.menu)
-        self.item_sub_item_1 = MenuItem.objects.create(title="sub_item_title_1", parent=self.item_menu_1,
-                                                       menu=self.menu)
 
-    def test_tag(self):
-        response = draw_menu('title-1', '')
-        expected = '''<li class="position-relative"><a href="#" class="btn btn-info menu__item menu__select">item_title_1</a><ul class="menu__inner_list"><li class="position-relative"><a href="default_url_item_menu" class="btn btn-success mb-1 menu__inner_item mb-1">sub_item_title_1</a></li></ul>'''
-        self.assertEquals(response, expected)
+    def test_draw_menu(self):
+        data = draw_menu(name_menu='title-1', current_url='/')
+        expected_data = '<li class="position-relative"><a href="default_url_item_menu/" class="btn btn-success menu__item">item_title_1</a></li><li class="position-relative"><a href="default_url_item_menu/" class="btn btn-success menu__item">item_title_1</a></li>'
+        self.assertEquals(data, expected_data)
+
+    def test_build_menu_structure(self):
+        query = MenuItem.objects.select_related('menu', 'parent').filter(menu__name=self.menu.name)
+        data = build_menu_structure(query)
+        expected_data = [{'id': 1, 'title': 'item_title_1', 'url': 'default_url_item_menu', 'menu': 'title-1'}]
+        self.assertEquals(data, expected_data)
